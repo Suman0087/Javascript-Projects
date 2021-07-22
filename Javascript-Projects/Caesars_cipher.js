a61f86f65b787b6b6bcb6269587f9308a6ea1089
Suman0087 <coder7257@gmail.com>
2021-07-22 18:37:18 +0530

    js-project

---
+++ Javascript-Projects/Caesars_cipher.js
@@ -0,0 +1,19 @@
+function rot13(str) 
+{
+  let deciphered =[];
+  
+  for (let i=0; i < str.length; i++)
+  {
+    let unicode = str[i].charCodeAt(0);
+    
+    if (unicode >= 65 && unicode <= 90)
+    {
+      unicode + 13 <= 90 ? unicode += 13: unicode = 65 + ((unicode + 13) % 59);  
+    }
+    
+    deciphered.push(String.fromCharCode(unicode));
+  }  
+
+
+  return deciphered.join("").toUpperCase();
+}
