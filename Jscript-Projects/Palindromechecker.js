8d28e26b137601755717979edf72bd290e143be2
Suman0087 <sumankr0087@gmail.com>
2021-07-22 18:47:06 +0530

    js-project

---
+++ Jscript-Projects/Palindromechecker.js
@@ -0,0 +1,9 @@
+function palindrome(str) {
+    var reg = /[\W_]/g;
+    var smallStr = str.toLowerCase().replace(reg, "");
+    var reversed = smallStr.split("").reverse().join("");
+    if(reversed === smallStr) 
+    return true;
+    return false;
+  }
+  palindrome("numbers");
